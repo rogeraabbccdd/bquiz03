f6eaf76aace865c897205b6f509947224a0c67ba
rogeraabbccdd <rogeraabbccdd@gmail.com>
2019-03-25 04:45:04 +0800

    Clean

--- sql.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$link = mysqli_connect("localhost", "root", "", "dbxx3");
-	mysqli_query($link, "set names utf8");
-	session_start();
-	$time=["14:00~16:00","16:00~18:00","18:00~20:00","20:00~22:00","22:00~24:00"];
-?>
